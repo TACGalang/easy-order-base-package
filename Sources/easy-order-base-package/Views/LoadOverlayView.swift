ace33636e239c6552745c6830dafe6e2e8faf0f6
Tristan Angelo C. Galang <tac.galang@gmail.com>
2021-07-12 16:00:59 +0800

    Fix other changes

--- Sources/easy-order-base-package/Views/LoadOverlayView.swift
+++ Sources/easy-order-base-package/Views/LoadOverlayView.swift
@@ -40,7 +40,7 @@ open class LoadOverlayView: UIView {
         
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
